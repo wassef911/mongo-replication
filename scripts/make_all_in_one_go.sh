6a614604e19d006ae68b57db4da613b4dc50f017
wassef911 <1.wassef911@gmail.com>
2023-02-04 13:51:32 +0100

    progress

---
+++ scripts/make_all_in_one_go.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+bash /scripts/make_replica_set.sh $REPLICA_SET_NAME_1 "mongo1_rs1" $DB1_RS1_PORT "mongo2_rs1" $DB2_RS1_PORT "mongo3_rs1" $DB3_RS1_PORT
+
+bash /scripts/make_replica_set.sh $REPLICA_SET_NAME_2 "mongo1_rs2" $DB1_RS2_PORT "mongo2_rs2" $DB2_RS2_PORT "mongo3_rs3" $DB3_RS2_PORT
+
+bash /scripts/make_replica_set.sh $REPLICA_SET_NAME_3 "mongo1_rs3" $DB1_RS3_PORT "mongo2_rs3" $DB2_RS3_PORT "mongo3_rs3" $DB3_RS3_PORT
--- scripts/make_replica_set.sh
+++ scripts/make_replica_set.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 
 echo "********** make_replica_set.sh **********"
-echo ${MONGODB1}
-until curl http://${MONGODB1}:${MONGODB1_PORT}/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+if [ "$#" -lt 7 ]; then
+  echo "Error: 7 parameters are required, $# were passed."
+  exit 1
+fi
+
+REPLICA_SET_NAME=$1
+PRIMARY=$2
+PRIMARY_PORT=$3
+SECONDARY_1=$4
+SECONDARY_1_PORT=$5
+SECONDARY_2=$6
+SECONDARY_2_PORT=$7
+
+echo "********** Setting up the " $REPLICA_SET_NAME " **********"
+
+until curl http://${PRIMARY}:${PRIMARY_PORT}/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
   printf '.'
   sleep 1
 done
 
 echo make_replica_set.sh time now: `date +"%T" `
-mongosh --host ${MONGODB1}:${MONGODB1_PORT} <<EOF
+mongosh --host ${PRIMARY}:${PRIMARY_PORT} <<EOF
 var cfg = {
   "_id": "${REPLICA_SET_NAME}",
   "members": [
     {
       "_id": 0,
-      "host": "${MONGODB1}:${MONGODB1_PORT}",
+      "host": "${PRIMARY}:${PRIMARY_PORT}",
       "priority": 2
     },
     {
       "_id": 1,
-      "host": "${MONGODB2}:${MONGODB2_PORT}",
+      "host": "${SECONDARY_1}:${SECONDARY_1_PORT}",
       "priority": 0
     },
     {
       "_id": 2,
-      "host": "${MONGODB3}:${MONGODB3_PORT}",
+      "host": "${SECONDARY_2}:${SECONDARY_2_PORT}",
       "priority": 0
     }
   ]
 };
 rs.initiate(cfg);
 rs.reconfig(cfg, {force: true});
-rs.secondaryOk();
-rs.conf();
+rs.status();
 EOF
 
-echo "It's Done!"
+echo "********** Replica set :" $REPLICA_SET_NAME "is done! 🚀🚀🚀🚀🚀🚀🚀🚀🚀 **********"
