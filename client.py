d244e9b8ccefb1628c1bbc9cab7b73e44d79a369
wassef911 <1.wassef911@gmail.com>
2023-02-17 09:48:26 +0100

    quick change

--- client.py
+++ client.py
@@ -1,8 +1,27 @@
-from pymongo import MongoClient
+import pymongo
+import redis
+import json
 
-client = MongoClient("mongodb://localhost:27117/")
+# Connect to Redis
+redis_client = redis.Redis(host='localhost', port=6379, db=0)
 
-db = client["database_name"]
+# Check Redis cache for data
+cache_key = 'cache_key'
+cache_data = redis_client.get(cache_key)
 
-print(f"Connected to MongoDB successfully: {db}")
-## TODO CONNECT TO REDIS 
+if cache_data:
+    # Data is in cache
+    result = json.loads(cache_data)
+else:
+    # Data is not in cache, query MongoDB
+    mongo_client = pymongo.MongoClient('mongodb://localhost:27017/')
+    db = mongo_client['mydatabase']
+    col = db['mycollection']
+    query = {'name': 'John'}
+    result = col.find(query)
+
+    # Cache the result in Redis
+    redis_client.set(cache_key, json.dumps(list(result)))
+
+for doc in result:
+    print(doc)
